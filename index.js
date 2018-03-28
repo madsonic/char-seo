8fca98c3506ab225c96ab89719db0a6605c1981d
Gerald <ngjingsiang@gmail.com>
2018-03-28 17:49:58 +0800

    Implement checking of tag existence

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
-const fs = require('fs');
-const path = require('path');
 const expect = require('chai').expect;
 let htmlparser2 = require('htmlparser2');
 
-const parser = new htmlparser2.Parser({
-  onopentag: function(tag, attributes) {
-    console.log(`tag open: ${tag}`);
-    console.log(attributes);
-    // switch (tag) {
-    //   case 'img':
-    //     console.log('img');
-    //     break;
-    //   case 'a':
-    //     console.log('a');
-    //     break;
-    //   case 'title':
-    //     console.log('title');
-    //     break;
-    //   default:
-    //     console.log(tag);
-    // }
-  },
-  onclosetag: function(tag) {
-    console.log(`tag close: ${tag}`);
-  }
-});
-
 class CharSeo {
-  constructor(filePath) {
+  constructor(filePath = '', data = '') {
     this.filePath = filePath;
     this.treePath = [];
     this.attributesPresent = [];
@@ -35,14 +10,15 @@ class CharSeo {
 
     this._checkAttrExistence = true;
     this._checkTagExistence = true;
+    this._htmlData = data;
   }
 
   get appear() {
     return this;
   }
 
-  tag(htmlTags) {
-    this.treePath = [...htmlTags];
+  tag(htmlTag) {
+    this.treePath = [htmlTag];
     return this;
   }
 
@@ -64,22 +40,80 @@ class CharSeo {
   }
 
   exist() {
-    let nodeStack =
+    const treePath = this.treePath;
+    let tagStack = [...treePath].reverse();
+    let undoStack = [];
+    let result = false;
+
     const parser = new htmlparser2.Parser({
-      onopentag: (tag, attr) {
+      onopentag: (tag, attr) => {
+        if (tag === tagStack[tagStack.length - 1]) {
+          undoStack.push(tagStack.pop());
+        }
 
+        // check if all target tags have been traversed
+        if (treePath.length !== 0 && undoStack.length === treePath.length) {
+          for (let i = 0; i < undoStack.length; ++i) {
+            // check for equality in case stack manipulation was incorrect
+            if (undoStack[i] !== treePath[i]) {
+              return;
+            }
+          }
+          result = true;
+        }
       },
-      onclosetag: (tag) {
-
+      onclosetag: (tag) => {
+        if (tag === undoStack[undoStack.length - 1]) {
+          tagStack.push(undoStack.pop());
+        }
       }
-    })
+    });
+
+    parser.write(this._htmlData); // need to change this to data
+    parser.end();
+
+    // reset state
+    this._reset();
+
+    return result;
+  }
+
+  /**
+   * Resets state so that another rule check can be performed on the same file/stream
+   */
+  _reset() {
+    this.treePath = [];
+    this.attributesPresent = [];
+    this.attributesAbsent = [];
+
+    this._checkAttrExistence = true;
+    this._checkTagExistence = true;
   }
 }
 
-const filePath = path.join(__dirname, 'test.html');
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  expect(true).to.be.true;
-});
+const testHtml = `
+<!DOCTYPE html>
+<html lang=en>
+<head>
 
-// parser.write();
-// parser.end();
+</head>
+<body>
+    <img alt='f'>
+    <h1>My First Heading</h1>
+    <p>My first paragraph.</p>
+    <div style='color:black'>
+        <div style='color:blue'>
+            <div style='color:green'>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+`;
+
+expect(new CharSeo('', testHtml).tag('div').exist()).to.be.true;
+expect(new CharSeo('', testHtml).tag('div').hasChild('div').exist()).to.be.true;
+expect(new CharSeo('', testHtml).tag('div').hasChildren(['div', 'div']).exist()).to.be.true;
+expect(new CharSeo('', testHtml).tag('h2').exist()).to.be.false;
+expect(new CharSeo('', testHtml).tag('img').with('alt').exist()).to.be.true;
